f6b19a10a0a4fe3c450278fade3db0348cac7e93
long123444 <longttggggtt@gmail.com>
2024-01-31 10:22:31 +0700

    suabai

---
+++ baitapthuchanh/chuoi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class chuoi {
+    public static void main(String[] args) {
+        String chuoi;
+    char kyTu;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhập vào chuỗi bất kỳ: ");
+    chuoi = scanner.nextLine();
+    System.out.println("Các ký tự có trong chuỗi là: ");
+    for (int i = 0; i < chuoi.length(); i++) {
+        kyTu = chuoi.charAt(i);
+        System.out.println(kyTu);
+    }
+}
+    }
