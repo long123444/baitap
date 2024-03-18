3794b356ae0b564d8b5430065d5ef0ca40b94f9b
long123444 <longttggggtt@gmail.com>
2024-03-18 21:10:07 +0700

    suabai

---
+++ baitapthuchanh/hinhhoc/tugiac.java
@@ -0,0 +1,17 @@
+package hinhhoc;
+
+public class tugiac {
+    public static void main(String[] args) {
+        public String tentugiac;
+        public int chieudai , int chieurong;
+    }
+} 
+public void {
+    Scanner sc = new Scanner
+    System.out.println ("ten tu giac:")
+    System.out.println ("chieuu dai :")
+    System.out.println ("chieuu rong :")
+}
+void tinhChuvi(){ cv = chieudai*chieurong};
+void tinhChuvi( String tentugiac, int chieudai, int chieurong)
+{ cv = chieudai*chieurong}
