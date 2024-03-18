dfc500d6ade7737a9af2fb5f6f64399c954ea17b
long123444 <longttggggtt@gmail.com>
2024-03-18 20:51:46 +0700

    6/3

---
+++ baitapthuchanh/hinhhoc/tugiac.java
@@ -0,0 +1,13 @@
+package hinhhoc;
+
+public class tugiac {
+    public static void main(String[] args) {
+        public String tentugiac;
+        public int chieudai , int chieurong;
+    }
+} 
+public void {
+    System.out.println ("ten tu giac:")
+    System.out.println ("chieuu dai :")
+    System.out.println ("chieuu rong :")
+}
