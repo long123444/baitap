a86816bdaa8166707b77fed715f111657aeeb8e7
long123444 <longttggggtt@gmail.com>
2024-05-15 09:29:10 +0700

    15/5

---
+++ baitap/arrrlish.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class arrrlish {
+    public static void main(String[] args) {
+    //     ArrayList<Integer> arrLish = new ArrayList<Integer>();
+    //     arrLish.set(0,1);
+    //     arrLish.set(5,6);
+    //     arrLish.set(6,1);
+    //     arrLish.set(0,9);
+    //     arrLish.set(6,1);
+    //     arrLish.set(0,1);
+    //     arrLish.set(65,6);
+    //     int sum =0;
+    //     for( int i=0; i<arrLish.size();i++)
+    //     {
+    //        sum+= arrLish.get(i);
+    //     }
+    //     System.out.println( " tong các phần tử :"+sum);
+    // }
+// ArrayList <Character> arrLish = new ArrayList<Character>();
+// arrLish.add('c');
+// arrLish.add('h');
+// arrLish.add('a');
+// arrLish.add('b');
+// arrLish.add('i');
+// ListIterator < Character> listIterator = arrLish.listIterator();
+// System.out.println(" các phần tử  có trong char là");
+
+// while (listIterator.hasNext())
+// {
+//     System.out.println(listIterator.hasNext() + "\t");
+// }
+}
+}
+
